145ebf6b9f4292e02c00809efc0fded005f43010
donaldcluck <dcluck@vrcnetwork.com>
2019-01-06 17:25:06 -0500

    Added mutation to delete inventory objects

--- src/connectors/typeorm/entity/Inventory.ts
+++ src/connectors/typeorm/entity/Inventory.ts
@@ -4,38 +4,38 @@ import { ObjectType, Field, ID, InputType, Int } from 'type-graphql';
 @Entity()
 @ObjectType()
 export class Inventory {
-    @PrimaryGeneratedColumn('uuid')
-    @Field(_type => ID)
-    readonly uid: string;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(_type => ID)
+  readonly uid: string;
 
-    @Column()
-    @Field()
-    name: string;
+  @Column()
+  @Field()
+  name: string;
 
-    @Column({ nullable: true })
-    @Field({ nullable: true })
-    description?: string;
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  description?: string;
 
-    @Column({ default: 0 })
-    @Field(_type => Int)
-    stock: number;
+  @Column({ default: 0 })
+  @Field(_type => Int)
+  stock: number;
 
-    @Column({ type: 'simple-array', nullable: true })
-    @Field(_type => [String], { nullable: true })
-    images: string[];
+  @Column({ type: 'simple-array', nullable: true })
+  @Field(_type => [String], { nullable: true })
+  images: string[];
 }
 
 @InputType()
 export class InventoryInput implements Partial<Inventory> {
-    @Field()
-    name: string;
+  @Field()
+  name: string;
 
-    @Field({ nullable: true })
-    description?: string;
+  @Field({ nullable: true })
+  description?: string;
 
-    @Field(_type => Int)
-    stock: number;
+  @Field(_type => Int, { nullable: true })
+  stock?: number;
 
-    @Field(_type => [String], { nullable: true })
-    images?: string[];
+  @Field(_type => [String], { nullable: true })
+  images?: string[];
 }
--- src/connectors/typeorm/resolver/InventoryResolver.ts
+++ src/connectors/typeorm/resolver/InventoryResolver.ts
@@ -7,25 +7,35 @@ import { Inventory, InventoryInput } from '../entity/Inventory';
 
 @Resolver(Inventory)
 export class InventoryResolver {
-    constructor(
-        @InjectRepository(Inventory) private readonly inventoryRepository: Repository<Inventory>
-    ) {}
+  constructor(
+    @InjectRepository(Inventory) private readonly inventoryRepository: Repository<Inventory>
+  ) {}
 
-    @Query(_returns => Inventory)
-    inventoryInfo(@Arg('uid') uid: string): Promise<Inventory> {
-        return this.inventoryRepository.findOneOrFail(uid);
-    }
+  @Query(_returns => Inventory)
+  inventoryInfo(@Arg('uid') uid: string): Promise<Inventory> {
+    return this.inventoryRepository.findOneOrFail(uid);
+  }
 
-    @Query(_returns => [Inventory])
-    inventoryFeed(@Args() { skip, take }: PaginationArgs): Promise<Inventory[]> {
-        return this.inventoryRepository.find({
-            skip,
-            take,
-        });
-    }
+  @Query(_returns => [Inventory])
+  inventoryFeed(@Args() { skip, take }: PaginationArgs): Promise<Inventory[]> {
+    return this.inventoryRepository.find({
+      skip,
+      take,
+    });
+  }
 
-    @Mutation(_returns => Inventory)
-    addInventory(@Arg('inventory') inventory: InventoryInput): Promise<Inventory> {
-        return this.inventoryRepository.save(inventory);
+  @Mutation(_returns => Inventory)
+  addInventory(@Arg('inventory') inventory: InventoryInput): Promise<Inventory> {
+    return this.inventoryRepository.save(inventory);
+  }
+
+  @Mutation(_returns => Inventory, { nullable: true })
+  async deleteInventory(@Arg('uid') uid: string): Promise<Inventory|null> {
+    try {
+      const inventory = await this.inventoryRepository.findOneOrFail(uid);
+      return this.inventoryRepository.remove(inventory);
+    } catch (error) {
+      return null;
     }
+  }
 }
